ff090f62b7885fc4fe03a8cfcbe5404f78baf9b8
way2muchnoise <jens.baetens@live.be>
2016-08-22 19:30:46 +0200

    uncomment all content in the book

--- src/demo/java/net/afterlifelochie/demo/GuiDemoBook.java
+++ src/demo/java/net/afterlifelochie/demo/GuiDemoBook.java
@@ -60,7 +60,7 @@ public class GuiDemoBook extends BookGUI
             PageProperties properties = new PageProperties(400, 450, new TextFormat(daniel));
             properties.headingFormat(new TextFormat(notethis, EnumSet.of(DecorationStyle.BOLD, DecorationStyle.ITALIC), new ColorFormat(255, 128, 64)));
             properties.bodyFormat(new TextFormat(notethis));
-            properties.bothMargin(2).lineHeightSize(25).spaceSize(4).densitiy(0.66f);
+            properties.bothMargin(2).lineHeightSize(20).spaceSize(4).densitiy(0.66f);
 
 			/* Build the document */
             Document document = new Document();
@@ -86,8 +86,8 @@ public class GuiDemoBook extends BookGUI
 
             document.push(new ImageItemStack(new ItemStack(Blocks.ANVIL, 1), 32, 32, FloatMode.LEFT));
             document.push(new Paragraph(new FormattedString(reallines.get(0))));
-            //document.push(new ImageItemStack(new ItemStack(Items.DIAMOND, 1), 32, 32, AlignmentMode.CENTER));
-            //document.push(new ImageItemStack(new ItemStack(Items.APPLE, 1), 32, 32, FloatMode.LEFT));
+            document.push(new ImageItemStack(new ItemStack(Items.DIAMOND, 1), 32, 32, AlignmentMode.CENTER));
+            document.push(new ImageItemStack(new ItemStack(Items.APPLE, 1), 32, 32, FloatMode.LEFT));
             document.push(new Paragraph(new FormattedString(reallines.get(1)).applyFormat(new TextFormat(daniel), 0)));
             document.push(new CompilerHint(HintType.PAGE_BREAK));
 
