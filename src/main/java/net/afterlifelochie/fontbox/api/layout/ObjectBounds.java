04a019dc26fd5224e9fe7a59bd3146abe1f91d40
way2muchnoise <jens.baetens@live.be>
2017-01-29 19:02:12 +0100

    document ObjectBounds

--- src/main/java/net/afterlifelochie/fontbox/api/layout/ObjectBounds.java
+++ src/main/java/net/afterlifelochie/fontbox/api/layout/ObjectBounds.java
@@ -2,10 +2,13 @@ package net.afterlifelochie.fontbox.api.layout;
 
 import net.afterlifelochie.fontbox.api.formatting.layout.FloatMode;
 
+/**
+ * Defines the bounds of an object
+ */
 public class ObjectBounds {
-    public int x, y;
-    public int width, height;
-    public FloatMode floating;
+    public final int x, y;
+    public final int width, height;
+    public final FloatMode floating;
 
     public ObjectBounds(int x, int y, int width, int height, FloatMode floating) {
         this.x = x;
@@ -15,24 +18,36 @@ public class ObjectBounds {
         this.floating = floating;
     }
 
+    /**
+     * An object is floating when {@link #floating} is not equal to {@link FloatMode#NONE}
+     *
+     * @return whether the object is floating or not
+     */
     public boolean floating() {
         return floating != FloatMode.NONE;
     }
 
-    public FloatMode floatMode() {
-        return floating;
-    }
-
+    /**
+     * Check if this object intersects with that object
+     *
+     * @param that the object to check intersection with
+     * @return true when the objects intersect
+     */
     public boolean intersects(ObjectBounds that) {
-        boolean flag0 = (x < that.x + that.width && x + width > that.x);
-        boolean flag1 = (y < that.y + that.height && y + height > that.y);
-        return flag0 && flag1;
+        return (x < that.x + that.width && x + width > that.x)
+            && (y < that.y + that.height && y + height > that.y);
     }
 
+    /**
+     * Check if this object encloses a point
+     *
+     * @param x x coord of the point
+     * @param y y coord of the point
+     * @return true when the objects encloses the point
+     */
     public boolean encloses(int x, int y) {
-        boolean flag0 = (this.x <= x && this.x + width >= x);
-        boolean flag1 = (this.y <= y && this.y + height >= y);
-        return flag0 && flag1;
+        return (this.x <= x && this.x + width >= x)
+            && (this.y <= y && this.y + height >= y);
     }
 
     @Override
@@ -40,6 +55,12 @@ public class ObjectBounds {
         return "[" + x + ", " + y + "] => [" + width + " x " + height + "]";
     }
 
+    /**
+     * Check if this object is inside the given bounds
+     *
+     * @param bounds bounds to be inside of
+     * @return true when this object is inside the given bounds
+     */
     public boolean inside(ObjectBounds bounds) {
         return bounds.encloses(x, y) && bounds.encloses(x + width, y + height);
     }
