6be03bba15ab198c80ef851fda4f2b28ffdf58ea
way2muchnoise <jens.baetens@live.be>
2017-01-30 20:35:20 +0100

    document IPage

--- src/main/java/net/afterlifelochie/fontbox/api/layout/IPage.java
+++ src/main/java/net/afterlifelochie/fontbox/api/layout/IPage.java
@@ -1,9 +1,40 @@
 package net.afterlifelochie.fontbox.api.layout;
 
-public interface IPage extends IContainer {
+import net.minecraft.client.renderer.GlStateManager;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A page of {@link IElement}s
+ */
+public interface IPage extends IContainer, Iterable<IElement> {
+    /**
+     * @return all elements on the page
+     */
     Iterable<? extends IElement> allElements();
 
+    /**
+     * Dynamic rendering means that the results should be saved to a display list
+     *
+     * @return all elements that require dynamic rendering
+     */
     Iterable<? extends IElement> dynamicElements();
 
+    /**
+     * Static elements can/should be save to a display list for performance reasons
+     * See {@link net.minecraft.client.renderer.GlStateManager#glNewList(int, int)}
+     * and {@link GlStateManager#glEndList()} for more info
+     *
+     * @return all elements that can be statically rendered
+     */
     Iterable<? extends IElement> staticElements();
+
+    @Override
+    default Iterator<IElement> iterator(){
+        List<IElement> list = new LinkedList<>();
+        allElements().forEach(list::add);
+        return list.iterator();
+    }
 }
